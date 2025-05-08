86962dd90ad8b862e26836b114e5f9df7bb811e1
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-05-08 04:47:10 -0300

    Aula Curso em video

---
+++ Aulas/Aula 11/ex012.js
@@ -0,0 +1,11 @@
+var hora = 10
+onslotchange.log(`Agora são exatamente ${hora} horas!`)
+if (hora <= 4) {
+    console.log('Boa madrugada')
+} else if (hora <= 12) {
+    console.log('Bom dia!')
+}else if (hora <= 18) {
+    console.log('Boa tarde!')
+}else if(hora <= 24) {
+    console.log('Boa noite')
+}
